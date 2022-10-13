75f21a46451ee1db4f08ad3b1f63ab914188839c
Carmyo <carmelodicristina@gmail.com>
2022-10-13 16:04:26 +0200

    create array match

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+$match = [
+    [
+       "homeTeam"  => "Team One",
+       "awayTeam" => "Team Two",
+       "homeTeamPoints" => "73",
+       "awayTeamPoints" => "34"
+
+    ],
+    [
+       "homeTeam"  => "Team Three",
+       "awayTeam" => "Team Four",
+       "homeTeamPoints" => "41",
+       "awayTeamPoints" => "84"
+
+    ],
+    [
+       "homeTeam"  => "Team Five",
+       "awayTeam" => "Team Six",
+       "homeTeamPoints" => "73",
+       "awayTeamPoints" => "47"
+
+    ]
+];
+var_dump($match);
+?>
+
+<p><? $match ?></p>
